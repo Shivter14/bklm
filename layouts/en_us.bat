87a41957b9160531527c09738e0e4c699461f392
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-10-26 14:10:22 -0700

    Added English Keyboard Layout

---
+++ layouts/en_us.bat
@@ -0,0 +1,3 @@
+@set "charset_L=                                                0123456789       abcdefghijklmnopqrstuvwxyz     0123456789☼+=-,/                                                                          ;-,/.=`                          [\]'"
+@set "charset_U=                                                )‼@#$%%ˆ&☼(       ABCDEFGHIJKLMNOPQRSTUVWXYZ     0123456789☼+=-./                                                                          :_<¿>+~                          {|}“"
+@set "charset_A="
